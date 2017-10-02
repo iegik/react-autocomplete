99a54eab00de84ced3a8eab002adf1ec3603d0de
Arturs Jansons <a.jansons@gmail.com>
2017-10-02 13:13:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Select from 'Select';
-import TextInput from 'TextInput';
-import List from 'List';
-import ListItem from 'ListItem';
+import Select from './Select';
+import TextInput from './TextInput';
+import List from './List';
+import ListItem from './ListItem';
 export default {
     Select,
     TextInput,
